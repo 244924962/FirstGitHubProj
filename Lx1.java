009ba7396a1172d18cc189270628255e619483f0
244924962 <76211512+244924962@users.noreply.github.com>
2020-12-18 06:51:13 -0600

    Add files via upload

---
+++ Lx1.java
@@ -0,0 +1,14 @@
+package lx;
+
+import org.eclipse.jdt.internal.compiler.parser.ParserBasicInformation;
+
+public class Lx1 {
+	
+	public static void main(String[] args) {
+		Integer i = null;
+		if(i==6) {
+			System.out.println(111);
+		}
+	}
+	
+}
